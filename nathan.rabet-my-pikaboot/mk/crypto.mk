99f0c88581d4a16029e98c8c1b7c71d8cf1a4b51
Nathan RABET <nathan.rabet@epita.fr>
2022-12-22 23:08:16 +0100

    Hot fixing Makefiles

--- nathan.rabet-my-pikaboot/mk/crypto.mk
+++ nathan.rabet-my-pikaboot/mk/crypto.mk
@@ -1,46 +1,60 @@
 # Bootloader signature
 $(BUILD)/$(BOOTLOADER_SIG_HEX): $(BUILD)/$(BOOTLOADER_SIG)
+	mkdir -p $(BUILD)
 	xxd -p -c 16 $< | sed 's/\(..\)/0x\1, /g' | sed '$$s/,$$//' > $@
 
 $(BUILD)/$(BOOTLOADER_SIG): $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG) $(BUILD)/$(BOOTLOADER_PEM_PRIVATE)
+	mkdir -p $(BUILD)
 	openssl dgst -sha256 -sign $(BUILD)/$(BOOTLOADER_PEM_PRIVATE) < $< > $@
 
 $(BUILD)/$(BOOTLOADER_DER_PUBLIC_HEX): $(BUILD)/$(BOOTLOADER_DER_PUBLIC)
+	mkdir -p $(BUILD)
 	xxd -p -c 16 $< | sed 's/\(..\)/0x\1, /g' | sed '$$s/,$$//' > $@
 
 $(BUILD)/$(BOOTLOADER_DER_PUBLIC): $(BUILD)/$(BOOTLOADER_PEM_PRIVATE)
+	mkdir -p $(BUILD)
 	openssl rsa -in $< -pubout -outform DER -out $@
 
 $(BUILD)/$(BOOTLOADER_PEM_PRIVATE):
+	mkdir -p $(BUILD)
 	openssl genrsa -out $@ 2048
 
 # Kernel signature
 $(BUILD)/$(KERNEL_SIG_HEX): $(BUILD)/$(KERNEL_SIG)
+	mkdir -p $(BUILD)
 	xxd -p -c 16 $< | sed 's/\(..\)/0x\1, /g' | sed '$$s/,$$//' > $@
 
 $(BUILD)/$(KERNEL_SIG): $(BUILD)/$(KERNEL) $(BUILD)/$(KERNEL_PEM_PRIVATE)
+	mkdir -p $(BUILD)
 	openssl dgst -sha256 -sign $(BUILD)/$(KERNEL_PEM_PRIVATE) < $< > $@ 
 
 $(BUILD)/$(KERNEL_DER_PUBLIC_HEX): $(BUILD)/$(KERNEL_DER_PUBLIC)
+	mkdir -p $(BUILD)
 	xxd -p -c 16 $< | sed 's/\(..\)/0x\1, /g' | sed '$$s/,$$//' > $@
 	
 $(BUILD)/$(KERNEL_DER_PUBLIC): $(BUILD)/$(KERNEL_PEM_PRIVATE)
+	mkdir -p $(BUILD)
 	openssl rsa -in $< -pubout -outform DER -out $@
 
 $(BUILD)/$(KERNEL_PEM_PRIVATE):
+	mkdir -p $(BUILD)
 	openssl genrsa -out $@ 2048
 
 # Encryption
 $(BUILD)/$(DECRYPTOR_ELF): $(BUILD)/$(BOOTLOADER_DER_PUBLIC_HEX) $(BUILD)/$(BOOTLOADER_SIG_HEX) $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG_KEY_HASH_HEX) $(OBJS_DECRYPT) $(LIB_TOMCRYPT) $(LIB_TOMMATH) $(LIB_GCC)
+	mkdir -p $(BUILD)
 	$(LD) $(LDFLAGS) -o $@ $(OBJS_DECRYPT) $(LIB_TOMCRYPT) $(LIB_TOMMATH) $(LIB_GCC)
 
 $(BUILD)/$(DECRYPTOR_IMG): $(BUILD)/$(DECRYPTOR_ELF)
+	mkdir -p $(BUILD)
 	$(CROSS_COMPILE)objcopy -O binary $< $@
 
 $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG_KEY):
+	mkdir -p $(BUILD)
 	openssl rand -out $@ 32
 
 $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG): $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG_KEY) $(BUILD)/$(KERNEL_DER_PUBLIC_HEX) $(BUILD)/$(KERNEL_SIG_HEX) $(BUILD)/$(BOOTLOADER_IMG)
+	mkdir -p $(BUILD)
 	openssl enc -aes-256-cbc \
 	-in $(BUILD)/$(BOOTLOADER_IMG) \
 	-out $@ \
@@ -49,12 +63,15 @@ $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG): $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG_KEY) $
 
 # Hash
 $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG_KEY_HASH): $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG_KEY)
+	mkdir -p $(BUILD)
 	openssl dgst -sha256 -binary -out $@ $<
 
 $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG_KEY_HASH_HEX): $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG_KEY_HASH)
+	mkdir -p $(BUILD)
 	xxd -p -c 16 $< | sed 's/\(..\)/0x\1, /g' | sed '$$s/,$$//' > $@
 
 $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG_KEY_TXT): $(BUILD)/$(ENCRYPTED_BOOTLOADER_IMG_KEY)
+	mkdir -p $(BUILD)
 	xxd -p -c 64 $< | sed 's/\(..\)/\1/g' > $@
 
 .PHONY: cleancrypto
--- nathan.rabet-my-pikaboot/mk/thirdparties.mk
+++ nathan.rabet-my-pikaboot/mk/thirdparties.mk
@@ -2,45 +2,57 @@ BUSYBOX=busybox-1.35.0
 LINUX=linux-5.19.17
 
 $(BUILD)/$(BUSYBOX):
+	mkdir -p $(BUILD)
 	curl -LO https://busybox.net/downloads/$(BUSYBOX).tar.bz2 --output $(BUILD)/$(BUSYBOX).tar.bz2
 	tar jxvf $(BUILD)/$(BUSYBOX).tar.bz2 -C $(BUILD)
 	cp cfg/busybox_cfg $(BUILD)/$(BUSYBOX)/.config
 
 $(BUILD)/$(LINUX):
+	mkdir -p $(BUILD)
 	wget https://cdn.kernel.org/pub/linux/kernel/v5.x/$(LINUX).tar.xz --output-document=$(BUILD)/$(LINUX).tar.xz
 	tar -Jxvf $(BUILD)/$(LINUX).tar.xz
 
 $(BUILD)/$(LINUX)/.config: $(BUILD)/$(LINUX)
+	mkdir -p $(BUILD)
 	cp cfg/linux_cfg $@
 
 $(BUILD)/$(LINUX)/arch/$(ARCH)/boot/Image: $(BUILD)/$(LINUX)/.config
+	mkdir -p $(BUILD)
 	cd $(BUILD)/$(LINUX)/ && make ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) Image -j $(NCORE) && cd ..
 
 $(BUILD)/thirdparty/$(ARCH):
+	mkdir -p $(BUILD)
 	mkdir -p $(BUILD)/thirdparty
 	mkdir -p $@
 
 $(BUILD)/$(KERNEL): $(BUILD)/$(LINUX)/arch/$(ARCH)/boot/Image $(BUILD)/thirdparty/$(ARCH)
+mkdir -p $(BUILD)
 	cp $< $@
 
 $(BUILD)/$(BUSYBOX)/.config: $(BUILD)/$(BUSYBOX)
+	mkdir -p $(BUILD)
 	cd $(BUILD)/$(BUSYBOX)/ && make ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) defconfig -j $(NCORE)
 	cp cfg/busybox_cfg $@
 
 $(BUILD)/$(BUSYBOX)/_install: $(BUILD)/$(BUSYBOX)/.config
+	mkdir -p $(BUILD)
 	cd $(BUILD)/$(BUSYBOX) && make ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) install -j $(NCORE)
 
 $(BUILD)/$(BUSYBOX)/initramfs: $(BUILD)/$(BUSYBOX)/_install
+	mkdir -p $(BUILD)
 	mkdir -pv $@
 	cd $@ && for i in bin sbin etc proc sys usr/bin usr/sbin; do mkdir -pv $$i; done && cp -a ../_install/* .
 
 $(BUILD)/$(BUSYBOX)/initramfs/init: $(BUILD)/$(BUSYBOX)/initramfs
+	mkdir -p $(BUILD)
 	cd $(BUILD)/$(BUSYBOX) && $(MAKE) ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) install
 	cp cfg/busybox_init $@
 	chmod +x $@
 
 $(BUILD)/$(BUSYBOX)/initramfs.cpio.gz: $(BUILD)/$(BUSYBOX)/initramfs/init
+	mkdir -p $(BUILD)
 	cd $(BUILD)/$(BUSYBOX)/initramfs && find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initramfs.cpio.gz
 
 $(BUILD)/$(INITRD): $(BUILD)/$(BUSYBOX)/initramfs.cpio.gz $(BUILD)/thirdparty/$(ARCH)
+	mkdir -p $(BUILD)
 	cp $< $@
