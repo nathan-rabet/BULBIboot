79fbb5a807a0c50d03fbeec07e5ad1c4582e4044
Nathan RABET <nathan.rabet@epita.fr>
2023-01-09 18:57:46 +0100

    Moving files tree

--- dependences.sh
+++ dependences.sh
@@ -2,18 +2,21 @@ if [ -x "$(command -v apt)" ]; then
     sudo apt update
     sudo apt install -y gcc-aarch64-linux-gnu
     sudo apt install -y bison
+    sudo apt install -y flex
     sudo apt install qemu-system-arm
     sudo apt install openssl
 elif [ -x "$(command -v pacman)" ]; then
     sudo pacman -Syu
     sudo pacman -S --needed --noconfirm gcc-aarch64-linux-gnu
     sudo pacman -S --needed --noconfirm bison
+    sudo pacman -S --needed --noconfirm flex
     sudo pacman -S --needed --noconfirm qemu
     sudo pacman -S --needed --noconfirm openssl
 elif [ -x "$(command -v dnf)" ]; then
     sudo dnf update
     sudo dnf install -y gcc-aarch64-linux-gnu
     sudo dnf install -y bison
+    sudo dnf install -y flex
     sudo dnf install -y qemu-system-arm
     sudo dnf install -y openssl
 elif [ -x "$(command -v yum)" ]; then
